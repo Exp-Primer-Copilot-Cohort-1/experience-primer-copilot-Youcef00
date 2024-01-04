9e6f370d05f68b7d7d261b0d9b25942814942aa6
Youcef Moukeut <57762672+Youcef00@users.noreply.github.com>
2024-01-04 22:02:23 +0000

    coomments

---
+++ comments.js
@@ -0,0 +1,91 @@
+// create web server that can listen to request from client
+// and send response back to the client
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// import the model
+const Comments = require('../models/comments');
+
+// create router object
+const commentRouter = express.Router();
+
+// use body-parser to parse data sent by client
+commentRouter.use(bodyParser.json());
+
+// configure router to handle different endpoints
+commentRouter.route('/')
+    // get all comments
+    .get((req, res, next) => {
+        // get all comments from database
+        Comments.find({})
+            .populate('author')
+            .then((comments) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(comments);
+            }, (err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            });
+    })
+    // add new comment
+    .post((req, res, next) => {
+        // create new comment document
+        Comments.create(req.body)
+            .then((comment) => {
+                console.log('Comment created ', comment);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(comment);
+            }, (err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            });
+    })
+    // update all comments
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation is not supported on /comments');
+    })
+    // delete all comments
+    .delete((req, res, next) => {
+        // delete all comments from database
+        Comments.deleteMany({})
+            .then((response) => {
+                console.log('Comments deleted ', response);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response);
+            }, (err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            });
+    });
+
+// configure router to handle different endpoints
+commentRouter.route('/:commentId')
+    // get a comment by id
+    .get((req, res, next) => {
+        // get all comments from database
+        Comments.findById(req.params.commentId)
+            .populate('author')
+            .then((comment) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(comment);
+            }, (err) => {
+                next(err);
+            })
+            .catch((err) => {
+                next(err);
+            });
+            // finish the web server route
+    }
+)
